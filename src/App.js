66b34385de069fcab87a5b617b7bec4acb26686e
AirinLind <113242888+AirinLind@users.noreply.github.com>
2025-02-07 13:56:32 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/categories' element={<CategoryPage/>}/> 
+        <Route path='/categories' element={<CategoryPage/>}/> 
         <Route path='/category/:id' element={<CategoryProductsPage/>}/>
         <Route path='/products/all' element={<ProductsListPage/>}/>
         <Route path='/products/:id' element={<ProductItemPage/>}/>
